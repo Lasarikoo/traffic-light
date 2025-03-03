984440f64d85f73c35742df3133ce2a3858148a2
Lasarikoo <lazaro@bega.ltd>
2025-03-03 13:02:49 +0000

    Finished

--- src/js/components/Lights.jsx
+++ src/js/components/Lights.jsx
@@ -1,18 +1,46 @@
 import React from "react";
 import { useState } from "react";
 
+
+
 function Lights() {
     const [ color, setColor] = useState("red");
-    
+    let lights = [ "red", "yellow", "green" ];
 
+    function colorChange () {
+        let currentColor = lights.indexOf(color);
+        let nextColor = (currentColor + 1) % lights.length;
+        setColor(lights[nextColor]);
+    }
 
-    return (
+    if (color === "purple") {
+        return (
         <div className="container traffic-light">
-            <button onClick={() => setColor("red")}><div className={`red-light ${color === "red" ? "selected" : ""}`}></div></button>
-            <button onClick={() => setColor("yellow")}><div className={`yellow-light ${color === "yellow" ? "selected" : ""}`}></div></button>
-            <button onClick={() => setColor("green")}><div className={`green-light ${color === "green" ? "selected" : ""}`}></div></button>
+            <button className={`red-light ${color === "red" ? "selected" : ""}`} onClick={() => setColor("red")}></button>
+            <button className={`yellow-light ${color === "yellow" ? "selected" : ""}`} onClick={() => setColor("yellow")}></button>
+            <button className={`green-light ${color === "green" ? "selected" : ""}`} onClick={() => setColor("green")}></button>
+            <button className={`purple-light ${color === "purple" ? "selected" : ""}`} onClick={() => setColor("purple")}></button>
+            <div>
+                <button onClick={colorChange}>Next</button>
+                <button onClick={() => setColor("purple")}>Add+</button>
+            </div> 
         </div>
-    );
+        )
+    } else {
+        return (
+            <div className="container traffic-light">
+                
+                <button className={`red-light ${color === "red" ? "selected" : ""}`} onClick={() => setColor("red")}></button>
+                <button className={`yellow-light ${color === "yellow" ? "selected" : ""}`} onClick={() => setColor("yellow")}></button>
+                <button className={`green-light ${color === "green" ? "selected" : ""}`} onClick={() => setColor("green")}></button>
+                <div>
+                    <button onClick={colorChange}>Next</button>
+                    <button onClick={() => setColor("purple")}>Add+</button>
+                </div> 
+            </div>
+        );
+    }
+    
 };
 
 export default Lights;
